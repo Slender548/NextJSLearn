038b2d66c84e2c099de4bc560e7d52c21db7627d
Slender548 <barkaloff.m@gmail.com>
2024-09-09 16:30:43 +0300

    first commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Inter, Truculenta, Lusitana } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+
+export const truculenta = Truculenta({ weight: "200", subsets: ["latin"] });
+
+export const lusitana = Lusitana({ weight: "400", subsets: ["latin"] });
